7565e363e9ff62dba833cfc59b17764ec9738655
DikshaHaresh Vanvari <dhv2108@columbia.edu>
2015-11-08 23:52:00 -0500

    Error handling on student microservice

--- student.js
+++ student.js
@@ -16,30 +16,65 @@ client.connect();
 
 
 
-exports.addStudent = function(req)
+exports.addStudent = function(req, res, callback)
 {
 console.log('Connected to database');
 console.log(req.body.sid);
-var query = client.query("insert into ms_student_tbl values($1, $2, $3, $4, $5, $6, $7)", [req.body.sid, req.body.fname, req.body.lname, req.body.phno, req.body.degree, req.body.year, req.body.address]);
-query.on('end', function(result) {
-console.log("Row successfully inserted");
+var query = client.query("insert into ms_student_tbl values($1, $2, $3, $4, $5, $6, $7)", [req.body.sid, req.body.fname, req.body.lname, req.body.phno, req.body.degree, req.body.year, req.body.address], function(err)
+{
+if(err)
+ {
+  console.log('Student already exists.');
+  res.status(409);
+ }
+ else
+ {
+ 	query.on('end', function(error, result) {
+     if(error){
+     res.status(500);	
+      }
+
+//No error
+else{
+	res.status(200);
+	console.log("Successfully added student.");
+    }    
+ });
+ }
+ callback(res);
 });
 }
 
-exports.addCoursetoStudent = function(req)
+exports.addCoursetoStudent = function(req,res,callback)
 {
-
 var lname = req.body.lname;
 var courseno = req.body.courseno;
-
 
 var queryForCheckingExistenceOfPair = client.query("select * from ms_student_course_tbl where courseno = $1 and lname = $2", [courseno, lname], function(err, result){
 rowCount = result.rows.length;
 if(rowCount == 0)
 {
-var query = client.query("insert into ms_student_course_tbl values($1, $2)", [lname,courseno]);
-query.on('end', function(result) {
-				// message =   {
+var query = client.query("insert into ms_student_course_tbl values($1, $2)", [lname,courseno], function(err)
+{
+
+	if(err)
+	{
+	console.log('Trying to add course to a student that does not exist');
+    res.status(417);
+	}
+	else
+	{
+		query.on('end', function(error, result) {
+
+			if(error)
+			{
+				res.status(500);
+			}
+			else
+			{
+				res.status(200);
+				console.log("Row successfully inserted");
+					// message =   {
 	   //                    "origin":"student" ,
 	   //                    "event":"course_added_to_student",
 				// 								"lname" : lname,
@@ -47,7 +82,14 @@ query.on('end', function(result) {
 	   //                };
 	   //       console.log(typeof(message.origin));
 	   //       publisher.publish('RI', JSON.stringify(message));
-console.log("Row successfully inserted");
+			}
+			
+
+});
+	}
+
+callback(res);
+
 });
 }
 });
@@ -60,15 +102,27 @@ var course_nos = [];
 var responseJson;
 console.log('Connected to database');
 console.log(req.params.student_id);
+var flag = false;
+
+
 var query = client.query("Select * from ms_student_tbl left outer join ms_student_course_tbl on (ms_student_tbl.lname = ms_student_course_tbl.lname) where ms_student_tbl.lname= $1", [req.params.student_id]);
 query.on('row', function(row) {
+	flag = true;
 	console.log(row.courseno);
 	course_nos.push(row.courseno);
 	responseJson = "{'fname':"+ row.fname + ", 'lname':" + req.params.student_id + ", 'sid':"+ row.sid + ", 'phno' :" + row.phno + ", 'degree_level':" + row.degree_level + ", 'year' :" + row.year + ", 'address':" + row.address + ", 'course_nos': " + course_nos + "}";
   });
 query.on('end', function(result){
+
+	if(flag)
+	{
 	console.log(responseJson);
 	res.json(responseJson);
+    }
+    else
+    {
+     res.json({message:'Student does not exist'});
+    }
 	callback(res);
 });
 }
@@ -99,37 +153,36 @@ var query = client.query(queryString, [student_lname], function(err)
   {
   console.log('error');
   res.status(400);
-  //callback(res);  
-}
+  callback(res); 
+  }
   else
   {
-  	query.on('end', function(error,result) {
+  	query.on('end', function(result) {
 
-//if error
-if(error){
-res.status(500);	
+  		console.log("rowcount" + result.rowCount);
+
+if(result.rowCount == 0)
+{
+	res.status(417);
+	callback(res);
 }
 
 //No error
 else{
-	res.status(200);
-	
+	res.status(200);	
 	console.log("Row successfully updated");
-}
-
-	
+	callback(res);
+   }	
 	//client.end();
 });
 }
-
-callback(res);
-
 });
+
 }
 
 
 
-exports.deleteStudent = function(req)
+exports.deleteStudent = function(req, res, callback)
 {
 
 	console.log('Connected to database to delete student');
@@ -145,24 +198,32 @@ exports.deleteStudent = function(req)
     console.log("Deleted from relationship");
 	var relationshipDeleteQuery = client.query(queryForStudentDatabase, [student_lname]);
 	relationshipDeleteQuery.on('end', function(result) {
-    console.log("Deleted from student table");
-		message =   {
-										"origin":"student" ,
-										"event":"student_removed_from_all",
-										"lname" : student_lname,
-										"courseno" : "All"
-								};
-			 console.log(typeof(message.origin));
-			 publisher.publish('RI', JSON.stringify(message));
-	console.log("Row successfully deleted");
+		if(result.rowCount == 0)
+		{
+			res.status(417);
+		}
+		else
+		{
+			res.status(200);
+			 console.log("Deleted from student table");
+		// message =   {
+		// 								"origin":"student" ,
+		// 								"event":"student_removed_from_all",
+		// 								"lname" : student_lname,
+		// 								"courseno" : "All"
+		// 						};
+		// 	 console.log(typeof(message.origin));
+		// 	 publisher.publish('RI', JSON.stringify(message));
+	 //         console.log("Row successfully deleted");
+		} 
+		callback(res);
 });
 });
-
 }
 
 
 
-exports.deleteCourseFromStudent = function(req)
+exports.deleteCourseFromStudent = function(req,res,callback)
 {
 
 var courseno = req.params.course_id;
